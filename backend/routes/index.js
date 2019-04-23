07e52d40131b990b37aa2cc11550f30c7c27e2fe
guptahardik17 <guptahardik17@gmail.com>
2019-04-23 11:28:36 +0530

    Added more conditions

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -20,21 +20,28 @@ router.post('/api/addtodo', (req, res) => {
     "attachments": [{"text":""}]
   }
 
-  connection.query("select todomessage,username from todo where teamid='"+obj.team_id+"' and channelid='"+obj.channel_id+"' and todomessage='"+obj.text+"' and status='pending';", function(err, rows){
-    if (rows.length>0) {
-      output.text = "This Todo is already in the channel's todo list.";
-      output.attachments[0].text = "Added by: @"+ rows[0].username;
-      res.send(output);
-    }
-    else {
-      connection.query("insert into todo(teamid,teamdomain,channelid,channelname,userid,username,todomessage,status) values('"+obj.team_id+"','"+obj.team_domain+"','"+obj.channel_id+"','"+obj.channel_name+"','"+obj.user_id+"','"+obj.user_name+"','"+obj.text+"','pending');", function(err, rows){
-        if (err) throw err;
-        output.text = "Added TODO for '"+obj.text+"'";
-        output.attachments[0].text = "This Todo has been added to the channel's TODOs list";
+  if(obj.text==""){
+    output.text = "Invalid Request";
+    output.attachments[0].text = "Please add a todo name. (eg. /addtodo Create Game)";
+  }
+  else{
+    connection.query("select todomessage,username from todo where teamid='"+obj.team_id+"' and channelid='"+obj.channel_id+"' and todomessage='"+obj.text+"' and status='pending';", function(err, rows){
+      if (rows.length>0) {
+        output.text = "This Todo is already in the channel's todo list.";
+        output.attachments[0].text = "Added by: @"+ rows[0].username;
         res.send(output);
-      });
-    }
-  });
+      }
+      else {
+        connection.query("insert into todo(teamid,teamdomain,channelid,channelname,userid,username,todomessage,status) values('"+obj.team_id+"','"+obj.team_domain+"','"+obj.channel_id+"','"+obj.channel_name+"','"+obj.user_id+"','"+obj.user_name+"','"+obj.text+"','pending');", function(err, rows){
+          if (err) throw err;
+          output.text = "Added TODO for '"+obj.text+"'";
+          output.attachments[0].text = "This Todo has been added to the channel's TODOs list";
+          res.send(output);
+        });
+      }
+    });
+
+  }
   console.log(output);
 });
 
@@ -77,20 +84,26 @@ router.post('/api/marktodo', (req, res) => {
     "attachments": [{"text":""}]
   }
 
-  connection.query("select todomessage,username,status from todo where teamid='"+obj.team_id+"' and channelid='"+obj.channel_id+"' and todomessage='"+obj.text+"' and status='pending';", function(err, rows){
-    if (rows.length>0) {
-      connection.query("update todo set status='done' where teamid='"+obj.team_id+"' and channelid='"+obj.channel_id+"' and todomessage='"+obj.text+"';", function(err, rows){
-        output.text = "Removed TODO for '"+obj.text+"'";
-        output.attachments[0].text = "This TODO has been marked as done.";
+  if(obj.text==""){
+    output.text = "Invalid Request";
+    output.attachments[0].text = "Please add a todo name. (eg. /marktodo Create Game)";
+  }
+  else{
+    connection.query("select todomessage,username,status from todo where teamid='"+obj.team_id+"' and channelid='"+obj.channel_id+"' and todomessage='"+obj.text+"' and status='pending';", function(err, rows){
+      if (rows.length>0) {
+        connection.query("update todo set status='done' where teamid='"+obj.team_id+"' and channelid='"+obj.channel_id+"' and todomessage='"+obj.text+"';", function(err, rows){
+          output.text = "Removed TODO for '"+obj.text+"'";
+          output.attachments[0].text = "This TODO has been marked as done.";
+          res.send(output);
+        });
+      }
+      else {
+        output.text = "TODO Not Found or Invalid";
+        output.attachments[0].text = "This Todo is not in the pending todo's channel list.";
         res.send(output);
-      });
-    }
-    else {
-      output.text = "TODO Not Found or Invalid";
-      output.attachments[0].text = "This Todo is not in the pending todo's channel list.";
-      res.send(output);
-    }
-  });
+      }
+    });
+  }
 
   console.log(output);
 });
